eb4f0a8b9b20e644f6bbf98e4a3e6df52ef50117
lw222gz_1 <lw222gz@student.lnu.se>
2015-09-08 14:10:02 +0000

    Added the clock.

---
+++ Login_1DV608-master/view/DateTimeView.php
@@ -0,0 +1,23 @@
+<?php
+
+class DateTimeView {
+
+
+	public function show() {
+		
+		//Sets the timezone to our timezone, otherwise the clock is 2 hours behind
+		//TODO: is there a more dynamic way to make this? Cant imagine it works globally
+		date_default_timezone_set('Europe/Stockholm');
+		
+		//To get 1st, 2nd, 3rd on date.
+		$day = date('jS');
+		
+		//To get digital time display
+		$time = date('H:i:s');
+		
+		$date = getdate();
+		$timeString = "$date[weekday] the $day of $date[month] $date[year], The time is $time"; //$date[hours]:$date[minutes]:$date[seconds]";
+
+		return '<p>' . $timeString . '</p>';
+	}
+}
